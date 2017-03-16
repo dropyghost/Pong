781c04823094b5e8263f49db883b6269d6b1796f
IagoQ <iagoquintero00@gmail.com>
2017-03-16 21:39:18 +0200

    fixed wrong hitbox for rect powerup

--- powerup.js
+++ powerup.js
@@ -4,7 +4,7 @@ function powerup() {
 	this.size = width / 15;
 	this.pos = createVector(random(0, width - this.size), random(height / 4, height / 4 * 3));
 	this.np = [-1, 1];
-	
+
 	//displays powerup depending on its class
 	this.display = function () {
 		if (this.life > 0) {
@@ -23,12 +23,16 @@ function powerup() {
 	//checks if ball hits power up
 	this.hit = function (ball) {
 		if (this.life > 0) {
-	
+			//test hit for circles
 			if (collideCircleCircle(ball.pos.x, ball.pos.y, ball.r * 2, this.pos.x, this.pos.y, this.size)) {
 				if (this.class == 0) {
 					ball.speed.mult(1.5);
 					this.life = 0;
 				}
+
+			}
+			//test hit for rects
+			if (collideRectCircle(this.pos.x, this.pos.y, this.size, this.size, ball.pos.x, ball.pos.y, ball.r * 2, ball.r * 2)) {
 				if (this.class == 1) {
 					ball.pos.x += random(this.np) * random(ball.r * 6, ball.r * 8);
 					ball.pos.y += ball.r;
